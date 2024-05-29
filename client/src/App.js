9f2622b409cb8a002e11b2110b8632341175982b
ABzioui <anas.job.493@gmail.com>
2024-05-29 12:18:43 +0100

    The Breakdown chart is added and works fine

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Geography from "scenes/geography";
 import Overview from "scenes/overview";
 import Monthly from "scenes/monthly";
 import Daily from "scenes/daily";
+import Breakdown from "scenes/breakdown";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -47,6 +48,7 @@ function App() {
               <Route path="/overview" element={<Overview />} />
               <Route path="/monthly" element={<Monthly />} />
               <Route path="/daily" element={<Daily />} />
+              <Route path="/breakdown" element={<Breakdown />} />
             </Route>
           </Routes>
         </ThemeProvider>
